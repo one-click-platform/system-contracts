8e0c59f5302fd8cee1811a8b52f02deb540cb5f4
zlobste <nkrayniuk@gmail.com>
2021-03-27 22:42:02 +0200

    Add feature to get all nft of owner

--- contracts/WERC721.sol
+++ contracts/WERC721.sol
@@ -3,9 +3,14 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract WERC721 is Ownable, ERC721 {
+    using SafeMath for uint256;
+
     mapping(address => bool) private eligibleUsers;
+    uint256 public totalSupply;
+
 
     constructor (address[] memory _eligibleUsers, string memory _name, string memory _symbol) ERC721(_name, _symbol) {
         for (uint256 i = 0; i < _eligibleUsers.length; i++) {
@@ -15,6 +20,28 @@ contract WERC721 is Ownable, ERC721 {
 
     function mint(address _to, uint256 _tokenId, bytes memory _data) public onlyEligibleUser(msg.sender) {
         _safeMint(_to, _tokenId, _data);
+        totalSupply = totalSupply.add(1);
+    }
+
+    function tokensOfOwner(address _ownerOfTokens) public view returns (uint256[] memory) {
+        uint256 _tokenCount = balanceOf(_ownerOfTokens);
+
+        if (_tokenCount == 0) {
+            return new uint256[](0);
+        }
+
+        uint256[] memory _ownerTokens = new uint256[](_tokenCount);
+        uint256 _totalSupply = totalSupply;
+        uint256 _resultIndex = 0;
+
+        for (uint256 _tokenId = 1; _tokenId <= _totalSupply; _tokenId++) {
+            if (ownerOf(_tokenId) == _ownerOfTokens) {
+                _ownerTokens[_resultIndex] = _tokenId;
+                _resultIndex++;
+            }
+        }
+
+        return _ownerTokens;
     }
 
     function switchUserPermissions(address _user) public onlyOwner {
